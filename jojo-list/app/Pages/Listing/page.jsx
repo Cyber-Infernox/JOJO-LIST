61eaa51fb4e1f4725c22eb2136026971ab3bd9d5
Cyber_Infernox <sayonkar@gmail.com>
2023-10-27 18:34:22 +0530

    Fetching...

--- jojo-list/app/Pages/Listing/page.jsx
+++ jojo-list/app/Pages/Listing/page.jsx
@@ -2,10 +2,32 @@ export const metadata = {
   title: "Listing",
 };
 
-const Listing = () => {
+// Style
+import "./Listing.css";
+
+async function getNameList() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: {
+      // revalidate: 30,
+      revalidate: 0,
+    },
+  });
+  return res.json();
+}
+
+const Listing = async () => {
+  const nameList = await getNameList();
+
   return (
     <div>
-      <h1>Listing...</h1>
+      <h2>Listing...</h2>
+      {nameList.map((jojo) => (
+        <div key={jojo.id}>
+          <a className="single">
+            <h3>{jojo.name}</h3>
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
